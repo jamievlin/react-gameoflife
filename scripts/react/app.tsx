e7744da982ef2b4524f748a019629be65bec7578
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-02-16 18:14:58 -0700

    SITE: Add grid for cells.

--- scripts/react/app.tsx
+++ scripts/react/app.tsx
@@ -1,8 +1,69 @@
-class App extends React.Component {
+interface AppState {
+    checkedState: boolean[][]
+};
+
+class App extends React.Component<{}, AppState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+                checkedState: Array.from({ length: 20 }).map((_, i) => {
+                    return Array.from({ length: 30 }).map((__, j) => Math.random() < 0.5 ? true : false)
+                })
+        };
+        // this.handleCellChange = this.handleCellChange.bind(this);
+    }
+
+    invert = () => {
+        this.setState(oldState => { return {
+            checkedState: oldState.checkedState.map(v => v.map(w => !w))
+        };});
+    }
+
+    /*
+    randomize = () => {
+        this.setState(oldState => {
+            return {
+                checkedState: oldState.checkedState.map(v => Math.random() < 0.5 ? true : false)
+            };
+        });
+    }
+    */
+
+    handleCellChange = (index: [number, number], checked: boolean) => {
+        console.log(`checked index: ${index}, checked=${checked}`);
+        let newArray = [...this.state.checkedState];
+        newArray[index[0]][index[1]] = checked;
+        this.setState(oldState => { return {
+            checkedState: newArray
+        };});
+
+    }
+
     render() {
         return (
-            <h1>hello world!</h1>
-        );
+        <>
+            <table><tbody>{
+                this.state.checkedState.map((v, i) => (
+                    <tr>
+                    { v.map((f: boolean, j: number) => (
+                            <th>
+                                <Cell
+                            key={[i,j].toString()} idx={[i,j]}
+                            initCheckedState={f}
+                            changeFn={this.handleCellChange}
+                            />
+                            </th>
+                            ))
+                    }
+                    </tr>))
+                    }</tbody></table>
+                <br />
+                number checked: {
+                    this.state.checkedState.reduce((ps, arr) =>
+                        ps + arr.filter(v => v).length, 0)}
+                <button onClick={this.invert}>invert</button>
+                {/* <button onClick={this.randomize}>randomize</button> */}
+        </>);
     }
 }
 
