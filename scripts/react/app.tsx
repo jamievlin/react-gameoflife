b2d0a940cfac215aa4eb07252d908de64070aa07
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-02-18 10:28:53 -0700

    WEB: Add Game of Life tick.

--- scripts/react/app.tsx
+++ scripts/react/app.tsx
@@ -1,14 +1,38 @@
 interface AppState {
     checkedState: boolean[][]
+    intervalId: number | undefined
 };
 
-class App extends React.Component<{}, AppState> {
-    constructor(props: any) {
+function get_or_default<T>(arr: T[][], idx: [number, number], defaultReturn: T) : T {
+    if (0 <= idx[0] && idx[0] < arr.length) {
+        if (0 <= idx[1] && idx[1] < arr[idx[0]].length) {
+            return arr[idx[0]][idx[1]]
+        }
+    }
+    return defaultReturn;
+}
+
+function countneighbors(arr: boolean[][], idx: [number, number]): number {
+    const searchIdx = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1], [0, 1],
+        [1, -1], [1, 0], [1, 1],
+    ];
+
+    let neighbors = searchIdx.map(([dx, dy]) =>
+        get_or_default(arr, [idx[0] + dx, idx[1] + dy], false));
+    return neighbors.filter(v => v).length;
+
+}
+
+class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
         super(props);
         this.state = {
-                checkedState: Array.from({ length: 20 }).map((_, i) => {
-                    return Array.from({ length: 30 }).map((__, j) => Math.random() < 0.5 ? true : false)
-                })
+                checkedState: Array.from({ length: props.width }).map((_, i) => {
+                    return Array.from({ length: props.height }).map((__, j) => Math.random() < 0.1 ? true : false)
+                }),
+                intervalId: undefined
         };
         // this.handleCellChange = this.handleCellChange.bind(this);
     }
@@ -17,6 +41,45 @@ class App extends React.Component<{}, AppState> {
         this.setState(oldState => { return {
             checkedState: oldState.checkedState.map(v => v.map(w => !w))
         };});
+    }
+
+    step = () => {
+        let newArray: boolean[][] = this.state.checkedState.map((varr, i) => {
+            let newSubArr = varr.map((v, j) => {
+                let neighborCount = countneighbors(this.state.checkedState, [i, j]);
+                if (neighborCount < 2 || neighborCount > 3) {
+                    return false;
+                } else if (neighborCount === 2) {
+                    return v;
+                } else { // neighborCount === 3
+                    return true;
+                }
+            });
+            return newSubArr;
+        });
+
+        console.log('hello?')
+        this.setState(oldState => { return {
+            checkedState: newArray
+        }; });
+    }
+
+    startTick = () => {
+        if (this.state.intervalId === undefined) {
+            let newIntervalId = setInterval(this.step, 100)
+            this.setState(_ => { return {
+                intervalId: newIntervalId
+            }});
+        }
+    }
+
+    stop = () => {
+        clearInterval(this.state.intervalId);
+        this.setState(_ => {
+            return {
+                intervalId: undefined
+            }
+        });
     }
 
     /*
@@ -32,6 +95,7 @@ class App extends React.Component<{}, AppState> {
     handleCellChange = (index: [number, number], checked: boolean) => {
         console.log(`checked index: ${index}, checked=${checked}`);
         let newArray = [...this.state.checkedState];
+        newArray[index[0]] = [...this.state.checkedState[index[0]]]
         newArray[index[0]][index[1]] = checked;
         this.setState(oldState => { return {
             checkedState: newArray
@@ -47,7 +111,7 @@ class App extends React.Component<{}, AppState> {
                     <tr>
                     { v.map((f: boolean, j: number) => (
                             <th>
-                                <Cell
+                            <Cell
                             key={[i,j].toString()} idx={[i,j]}
                             initCheckedState={f}
                             changeFn={this.handleCellChange}
@@ -61,7 +125,10 @@ class App extends React.Component<{}, AppState> {
                 number checked: {
                     this.state.checkedState.reduce((ps, arr) =>
                         ps + arr.filter(v => v).length, 0)}
-                <button onClick={this.invert}>invert</button>
+                <button onClick={this.step}>step</button>
+                <button onClick={this.startTick}>start</button>
+                <button onClick={this.stop}>stop</button>
+
                 {/* <button onClick={this.randomize}>randomize</button> */}
         </>);
     }
